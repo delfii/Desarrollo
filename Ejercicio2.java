e5551f7d040fb38a588c9858ad0a4c17d626f9bd
delfii <81364432+delfii@users.noreply.github.com>
2021-03-25 09:48:17 -0300

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfiita;
+
+/**
+ *
+ * @author pc01
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a= 10;
+        int b=5;
+        int suma= a+b;
+        int resta= a-b;
+        int multiplicacion= a*b;
+        int division= a/b;
+        System.out.println("la sema de " +a+ " mas " +b+ " es igual a "+suma);
+        System.out.println("la resta de " +a+ " mas " +b+ " es igual a " +resta);
+        System.out.println("la multiplicacion de " +a+ " mas" +b+ " es igual a " +multiplicacion);
+        System.out.println("la division de " +a+ " mas " +b+ " es igual a " +division);
+        
+    }
+    
+}
