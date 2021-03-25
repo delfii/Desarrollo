a42f43c4fefb5d6f06778b726057dd9c471cf6cc
delfii <81364432+delfii@users.noreply.github.com>
2021-03-25 10:27:19 -0300

    Add files via upload

---
+++ ejercicio4.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfiita;
+import java.util.Scanner; 
+/**
+ *
+ * @author pc01
+ */
+public class ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada= new Scanner(System.in);
+        int horas;
+        int costo;
+        int mes;
+        int año;
+        System.out.println("ingrese horas trabajadas y el costo");
+        horas= entrada.nextInt();
+        costo= entrada.nextInt();
+        mes= horas*costo;
+        System.out.println("el sueldo por meses "+mes);
+        año= mes*12;
+        System.out.println("el sueldo por año es "+año);
+        
+    }
+    
+}
