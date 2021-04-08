b16918c8550a68d08ab304c6c1d7ae1d0e425e6b
delfii <81364432+delfii@users.noreply.github.com>
2021-04-08 10:03:25 -0300

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfiita;
+
+/**
+ *
+ * @author pc01
+ */
+import java.util.Scanner;
+public class Dowhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada= new Scanner(System.in);
+        int numero;
+        System.out.println("ingrese un numero y se contara hasta el mismo");
+        numero= entrada.nextInt();
+        int i=1;
+        do{System.out.println(i);
+        i++;
+        }
+        while (i<=numero);
+    }
+    
+}
