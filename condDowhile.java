ec702c5fcfa4a3681a9868679fb4b2d05e528994
delfii <81364432+delfii@users.noreply.github.com>
2021-04-08 09:59:46 -0300

    Add files via upload

---
+++ condDowhile.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfiita;
+
+/**
+ *
+ * @author pc01
+ */
+import java.util.Scanner; 
+public class condDowhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("numeros del 1 al 100");
+        int i=1;
+        do{System.out.println(i);
+        i++;
+        }
+        while (i<=100);
+    }
+    
+}
