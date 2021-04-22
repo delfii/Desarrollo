de69d52cfb066a737232300de950317e91021bda
delfii <81364432+delfii@users.noreply.github.com>
2021-04-22 10:20:39 -0300

    Add files via upload

---
+++ whileingresado.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfiita;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc01
+ */
+public class whileingresado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada= new Scanner (System.in);
+        int num=0;
+        System.out.println("ingrese un numero maximo");
+        num=entrada.nextInt();
+        int a=0;
+        while (a<=num){
+            if (a%6==0){
+                System.out.println(a);
+            }
+            a++;
+        }
+        
+        
+    }
+    
+}
